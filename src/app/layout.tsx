d78fbef6cccf29961c630a5b56c3efab2450cb4d
Liam Hales <liam.hales96@gmail.com>
2025-11-10 18:52:35 +0000

    Added `AppLayout` used as the entry point to the app

---
+++ src/app/layout.tsx
@@ -0,0 +1,41 @@
+import './globals.css';
+
+import { FunctionComponent, ReactElement, ReactNode } from 'react';
+import { BaseProps } from '../types';
+import { cascadiaCode } from '../fonts';
+import { viewport } from './metadata';
+import { App } from '../components';
+
+/**
+ * The `AppLayout` component props
+ */
+interface Props extends BaseProps {
+  readonly children: ReactNode;
+}
+
+/**
+ * The root layout component used as the entry point to the app which renders
+ * the main `App` component and configures things such as fonts and app state
+ *
+ * @param props The component props
+ * @returns The `AppLayout` component
+ */
+const AppLayout: FunctionComponent<Props> = ({ children }): ReactElement<Props> => {
+  return (
+    <html
+      lang="en"
+      className={`h-full ${cascadiaCode.variable} overscroll-none`}
+    >
+      <body className="h-full bg-black touch-none">
+        <App>
+          {children}
+        </App>
+      </body>
+    </html>
+  );
+};
+
+export default AppLayout;
+export {
+  viewport,
+};
