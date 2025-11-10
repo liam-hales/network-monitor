74278dc25ecdab13b7d8b44d4ea7272804e15c86
Liam Hales <liam.hales96@gmail.com>
2025-11-10 22:13:50 +0000

    Updated `App` component to render `Sidebar` component and resolve the host network info

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,10 +1,7 @@
-'use client';
-
 import { FunctionComponent, ReactElement, ReactNode } from 'react';
 import { BaseProps } from '../types';
-import { appNavItems } from '../constants';
-import { usePathname } from 'next/navigation';
-import Link from 'next/link';
+import { getHostNetworkInfo } from '../helpers';
+import { Sidebar } from './';
 
 /**
  * The `App` component props
@@ -20,50 +17,14 @@ interface Props extends BaseProps {
  * @returns The `App` component
  */
 const App: FunctionComponent<Props> = ({ children }): ReactElement<Props> => {
-  const pathname = usePathname();
+  const { address, mac } = getHostNetworkInfo();
 
   return (
     <div className="h-full flex flex-row items-start">
-      <div className="h-full flex flex-col items-start gap-y-10 pt-8 pb-8 pl-4 pr-4 border-solid border-r-[1px] border-neutral-900">
-        <div className="flex flex-col items-start gap-y-1 pl-4 pr-8">
-          <p className="font-mono text-white text-xl">
-            Network Monitor
-          </p>
-          <p className="font-mono text-neutral-500 text-xs">
-            192.168.0.1
-          </p>
-        </div>
-        <div className="w-full flex flex-col items-start gap-y-2">
-          {
-            appNavItems.map((item) => {
-              const { name, path } = item;
-              return (
-                <Link
-                  className="w-full"
-                  key={`sidebar-menu-item-${name}`}
-                  href={path}
-                  passHref={true}
-                  aria-label={name}
-                >
-                  <div className={`
-                    w-full flex flex-row items-center border-solid border-[1px] rounded-sm cursor-pointer gap-x-4 pt-2 pb-2 pl-4
-
-                    hover:bg-neutral-900
-
-                    ${(pathname === path) ? 'bg-neutral-900' : 'bg-transparent'}
-                    ${(pathname === path) ? 'border-neutral-800' : 'border-transparent'}
-                  `}
-                  >
-                    <p className="font-mono text-white text-sm">
-                      {name}
-                    </p>
-                  </div>
-                </Link>
-              );
-            })
-          }
-        </div>
-      </div>
+      <Sidebar
+        ipAddress={address}
+        macAddress={mac}
+      />
       <div className="flex flex-col items-center p-8">
         {children}
       </div>
