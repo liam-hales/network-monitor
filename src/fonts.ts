91e898eba307c625e6724c5169db51bd49be8b63
Liam Hales <liam.hales96@gmail.com>
2025-11-10 18:41:26 +0000

    Added Cascadia Code font using `next/font`

---
+++ src/fonts.ts
@@ -0,0 +1,14 @@
+import { Cascadia_Code } from 'next/font/google';
+
+/**
+ * The Cascadia Code font from Google Fonts
+ * self-hosted by `next/font`
+ *
+ * @url https://fonts.google.com/specimen/Cascadia+Mono
+ */
+export const cascadiaCode = Cascadia_Code({
+  variable: '--mono-font',
+  subsets: ['latin'],
+  style: ['normal'],
+  weight: ['400', '700'],
+});
