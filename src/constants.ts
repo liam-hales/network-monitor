8c4eece5cf6e94f7a514cdb3d46bd43e62c78bc3
Liam Hales <liam.hales96@gmail.com>
2025-11-12 22:57:55 +0000

    Added `subItems` prop to app nav items

--- src/constants.ts
+++ src/constants.ts
@@ -6,13 +6,29 @@ export const appNavItems = [
   {
     name: 'Overview',
     path: '/',
+    subItems: [
+      {
+        name: 'Network Speed',
+        path: '/network-speed',
+      },
+      {
+        name: 'Latency',
+        path: '/latency',
+      },
+      {
+        name: 'Jitter',
+        path: '/jitter',
+      },
+    ],
   },
   {
     name: 'Speed Test',
     path: '/speedtest',
+    subItems: [],
   },
   {
     name: 'Devices',
     path: '/devices',
+    subItems: [],
   },
 ] as const;
