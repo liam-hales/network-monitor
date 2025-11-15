2c72925a5d2bbdf4e35e5a42f27e30a8daa89a84
Liam Hales <liam.hales96@gmail.com>
2025-11-15 18:12:01 +0000

    Added `instrumentation-client.ts` file used to execute client-side setup code before the app becomes interactive

---
+++ src/instrumentation-client.ts
@@ -0,0 +1,8 @@
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+// Configure Day.js to use the UTC
+// and relative time plugins
+dayjs.extend(utc);
+dayjs.extend(relativeTime);
