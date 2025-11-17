ba49b7aeeb138e9f257cc2cf7cee22574b4ab41b
Liam Hales <liam.hales96@gmail.com>
2025-11-17 13:23:30 +0000

    Updated `performanceTest` server action to return an `AsyncGenerator` instead of a stream

--- src/server/actions/performanceTest.action.ts
+++ src/server/actions/performanceTest.action.ts
@@ -1,52 +1,84 @@
 'use server';
 
-import { StreamableValue, createStreamableValue } from '@ai-sdk/rsc';
 import { PerformanceTestResults } from '../types';
 import SpeedTest from '@cloudflare/speedtest';
 
 /**
- * Used to perform a network performance test and
- * stream the data in real-time to the client
+ * Used to perform a network performance test
+ * and `yield` the data as the test runs
  *
- * @returns The client streamable value
+ * @returns The data async generator
  */
-const performanceTest = async (): Promise<StreamableValue<PerformanceTestResults>> => {
-  // Define the new stream used to send data to the client
-  // Define the speed test client used to perform the speed test
-  const stream = createStreamableValue<PerformanceTestResults>();
+const performanceTest = async function* (): AsyncGenerator<PerformanceTestResults> {
+  let resolveNext: ((value: [PerformanceTestResults, boolean]) => void) | undefined;
+
+  // Define the speed test client used to
+  // perform the network performance test
   const client = new SpeedTest({
     autoStart: false,
   });
 
-  client.onResultsChange = () => {
+  /**
+   * Used as the callback for the `SpeedTest` client which is
+   * called when results change and when the test has completed
+   *
+   * @param isFinal Determines if the callback is for the final results
+   */
+  const _callback = (isFinal: boolean) => {
     const results = client.results.getSummary();
 
-    // Update the stream with the result
-    // values from the speed test
-    stream.update({
-      idle: {
-        bandwidth: 0,
-        latency: results.latency ?? 0,
-        jitter: results.jitter ?? 0,
+    // Skip if the resolve next function
+    // has not been defined yet
+    if (resolveNext == null) {
+      return;
+    }
+
+    // Yield the result data
+    // from the summary
+    resolveNext([
+      {
+        idle: {
+          bandwidth: 0,
+          latency: results.latency ?? 0,
+          jitter: results.jitter ?? 0,
+        },
+        download: {
+          bandwidth: results.download ?? 0,
+          latency: results.downLoadedLatency ?? 0,
+          jitter: results.downLoadedJitter ?? 0,
+        },
+        upload: {
+          bandwidth: results.upload ?? 0,
+          latency: results.upLoadedLatency ?? 0,
+          jitter: results.upLoadedJitter ?? 0,
+        },
       },
-      download: {
-        bandwidth: results.download ?? 0,
-        latency: results.downLoadedLatency ?? 0,
-        jitter: results.downLoadedJitter ?? 0,
-      },
-      upload: {
-        bandwidth: results.upload ?? 0,
-        latency: results.upLoadedLatency ?? 0,
-        jitter: results.upLoadedJitter ?? 0,
-      },
-    });
+      isFinal,
+    ]);
   };
 
-  // Tell the client to start
-  // the network speed test
+  // Set client functions to call the `_callback` and
+  // start the network performance test
+  client.onResultsChange = () => _callback(false);
+  client.onFinish = () => _callback(true);
   client.play();
 
-  return stream.value;
+  while (true) {
+
+    // Await on a new promise and assign its `resolve`
+    // to `resolveNext` to wait for the new data
+    const [value, isFinal] = await new Promise<[PerformanceTestResults, boolean]>((resolve) => {
+      resolveNext = resolve;
+    });
+
+    yield value;
+
+    // If these are the final results
+    // then break out the loop
+    if (isFinal === true) {
+      break;
+    }
+  }
 };
 
 export default performanceTest;
