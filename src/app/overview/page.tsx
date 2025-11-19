cec4ea4f133e0c5e6e39a1a580ae7228228eeab8
Liam Hales <liam.hales96@gmail.com>
2025-11-19 18:39:58 +0000

    Updated `OverviewPage` component to be `async` and added logic to fetch the data for the charts

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -1,5 +1,8 @@
-import { FunctionComponent, ReactElement } from 'react';
+import { ReactElement } from 'react';
 import { SummaryPanel, SpeedPanel, LatencyPanel } from '../../components';
+import { dbFind } from '../../server/actions';
+import { AsyncComponent } from '../../types';
+import { PerformanceEntity } from '../../server/types';
 
 /**
  * The entry point for the `/overview` app route,
@@ -7,24 +10,61 @@ import { SummaryPanel, SpeedPanel, LatencyPanel } from '../../components';
  *
  * @returns The `OverviewPage` component
  */
-const OverviewPage: FunctionComponent = (): ReactElement => {
+const OverviewPage: AsyncComponent = async (): Promise<ReactElement> => {
+  const data = await dbFind<PerformanceEntity>({
+    collectionName: 'network_performance',
+    query: {},
+  });
+
+  /**
+   * The mapped chart data for
+   * the `SummaryPanel` component
+   */
+  const speedChartData = data.map((item) => {
+    const { date, download, upload } = item;
+
+    return {
+      date: date,
+      download: Math.round(download.bandwidth / 1_000_000),
+      upload: Math.round(upload.bandwidth / 1_000_000),
+    };
+  });
+
+  /**
+   * The mapped chart data for
+   * the `LatencyPanel` component
+   */
+  const latencyChartData = data.map((item) => {
+    const { date, idle } = item;
+
+    return {
+      date: date,
+      idle: Math.round(idle.latency),
+    };
+  });
+
+  const [latestSpeed] = [...speedChartData].reverse();
+  const [latestLatency] = [...latencyChartData].reverse();
+
   return (
     <div className="w-full flex flex-col items-center gap-y-6">
       <SummaryPanel
         className="w-full"
-        download={0}
-        upload={0}
-        latency={0}
+        download={latestSpeed.download}
+        upload={latestSpeed.download}
+        latency={latestLatency.idle}
         jitter={0}
       />
       <SpeedPanel
         className="w-full h-[300px]"
-        chartData={[]}
+        chartData={speedChartData}
       />
-      <LatencyPanel
-        className="w-full h-[300px]"
-        chartData={[]}
-      />
+      <div className="w-full flex flex-row items-center justify-between gap-x-6">
+        <LatencyPanel
+          className="w-full h-[300px]"
+          chartData={latencyChartData}
+        />
+      </div>
     </div>
   );
 };
